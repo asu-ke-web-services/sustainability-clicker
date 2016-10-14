aedd64b4c780abefed40cb26586302f1ce5e1ca6
Nathan Rollins <nathan.rollins@asu.edu>
2016-10-14 10:08:13 -0700

    Add scaffolding dot files
    Forgot to copy these in.

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "jquery": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ]
+  }
+};
