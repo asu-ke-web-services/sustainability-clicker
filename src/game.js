9b5c7c5adf8fd77c9f4b4da4b6778d245aca4ee7
apourdeh <arminpourdehghan@gmail.com>
2016-10-19 14:23:35 -0700

    test

--- src/game.js
+++ src/game.js
@@ -3,3 +3,21 @@ var game_state = require("./game_state.js");
 
 
 $(".hello_text").html(game_state.sayHelloInEnglish());
+
+var carbNUm = 0;
+
+var element = {
+    clicker : document.getElementById("clicker");
+    points : document.getElementById("pointer");
+}
+
+element.clicker.onclick = function(){
+    updatePoints();
+}
+
+Function()
+{
+    carbNUm = carbNUm+ carbNUm;
+    thistext = "NUmber Of Carbons: " + carbNUm;
+    element.points.innerHTML = thistext;
+}
