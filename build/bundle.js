9454b60d77f98e3860bab817c7539f694bda868b
Ryan Raub <ryan.raub@asu.edu>
2016-10-19 17:07:02 -0700

    moved game.js into src dir and built new version also moved css into style.css

--- build/bundle.js
+++ build/bundle.js
@@ -45,11 +45,37 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	var $ = __webpack_require__(1);
-	var game_state = __webpack_require__(2);
+	// var game_state = require("./game_state.js");
 
 
-	$(".hello_text").html(game_state.sayHelloInEnglish());
+	var carbNum = 0;
+	var adder = 1;
 
+	var element = {   
+	  clicker:document.getElementById("clicker"),
+	  points:document.getElementById("points"),
+	  purchaseTree1 : document.getElementById("purchaseTree1"),  
+	};
+
+	element.clicker.onclick = function() { updatePoints(); };
+	element.purchaseTree1.onclick = function() { up(); };
+
+	function updatePoints()
+	{
+	 carbNum = carbNum + adder;
+	 text = "Number Of Carbons: " + carbNum;
+	 element.points.innerHTML = text;
+	}
+
+	function up()
+	{
+	  if (carbNum >= 3)
+	    {
+	     carbNum =  carbNum - 3;
+	      element.points.innerHTML = "Number Of Carbons: " + carbNum;
+	  adder = adder + 1;
+	    }
+	}
 
 /***/ },
 /* 1 */
@@ -10275,21 +10301,6 @@
 
 	return jQuery;
 	} );
-
-
-/***/ },
-/* 2 */
-/***/ function(module, exports) {
-
-	module.exports = {
-	  sayHelloInEnglish: function() {
-	    return "HELLO";
-	  },
-
-	  sayHelloInSpanish: function() {
-	    return "Hola";
-	  }
-	};
 
 
 /***/ }
--- src/game.js
+++ src/game.js
@@ -1,5 +1,32 @@
 var $ = require("jquery");
-var game_state = require("./game_state.js");
+// var game_state = require("./game_state.js");
 
 
-$(".hello_text").html(game_state.sayHelloInEnglish());
+var carbNum = 0;
+var adder = 1;
+
+var element = {   
+  clicker:document.getElementById("clicker"),
+  points:document.getElementById("points"),
+  purchaseTree1 : document.getElementById("purchaseTree1"),  
+};
+
+element.clicker.onclick = function() { updatePoints(); };
+element.purchaseTree1.onclick = function() { up(); };
+
+function updatePoints()
+{
+ carbNum = carbNum + adder;
+ text = "Number Of Carbons: " + carbNum;
+ element.points.innerHTML = text;
+}
+
+function up()
+{
+  if (carbNum >= 3)
+    {
+     carbNum =  carbNum - 3;
+      element.points.innerHTML = "Number Of Carbons: " + carbNum;
+  adder = adder + 1;
+    }
+}
