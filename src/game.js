d6bae23487786491e4bd0344af1f9fbfee781f53
apourdeh <arminpourdehghan@gmail.com>
2016-10-21 17:54:27 -0700

    disabled button - digit formating -
    Added disable functionality to disable purchase buttons until players
    reached the price - formating the digits - added a basic formula to
    calculate the numebr of carbons needed to buy item 1 after each purchase

--- src/game.js
+++ src/game.js
@@ -1,11 +1,14 @@
-var $ = require("jquery");
-// var game_state = require("./game_state.js");
+// var $ = require("jquery");
+var game_state = require("./game_state.js");
 
 
 var carbNum = 0;
 var adder = 1;
+var purchaseLevel = 1;
+var treePurchaseLevel = 3;
 
-var element = {   
+var element = {  
+    
   clicker:document.getElementById("clicker"),
   points:document.getElementById("points"),
   purchaseTree1 : document.getElementById("purchaseTree1"),  
@@ -14,19 +17,42 @@ var element = {
 element.clicker.onclick = function() { updatePoints(); };
 element.purchaseTree1.onclick = function() { up(); };
 
+element.purchaseTree1.innerHTML = "Tree : unlock on " + treePurchaseLevel;
+
 function updatePoints()
 {
+    var carbNumftm;
+    
  carbNum = carbNum + adder;
- text = "Number Of Carbons: " + carbNum;
- element.points.innerHTML = text;
+    carbNumftm = game_state.addCommas(carbNum)
+ element.points.innerHTML = "Number Of Carbons: " + carbNumftm;
+    
+    if(carbNum >= treePurchaseLevel)
+        {
+              document.getElementById("purchaseTree1").disabled = false;
+        }
 }
+
 
 function up()
 {
-  if (carbNum >= 3)
+    treePurchaseLevel;
+    var treePurchaseLevelFormating;
+    var carbNumFormating;
+  if (carbNum >= treePurchaseLevel)
     {
-     carbNum =  carbNum - 3;
-      element.points.innerHTML = "Number Of Carbons: " + carbNum;
-  adder = adder + 1;
+        purchaseLevel++;
+     carbNum =  carbNum - treePurchaseLevel;
+     carbNumFormating = game_state.addCommas(carbNum);
+        element.points.innerHTML = "Number Of Carbons: " + carbNumFormating;
+  adder = treePurchaseLevel;
+        treePurchaseLevel = purchaseLevel*(treePurchaseLevel);
+        treePurchaseLevelFormating = game_state.addCommas(treePurchaseLevel);
+         element.purchaseTree1.innerHTML = "Tree : unlock on " + treePurchaseLevelFormating;
+      if(carbNum <= treePurchaseLevel)
+          {
+              document.getElementById("purchaseTree1").disabled = true;
+          }
+        
     }
 }
