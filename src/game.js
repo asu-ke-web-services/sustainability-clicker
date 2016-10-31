35b9f270e819c3f8f5b685e4535da0bbbc647f8a
apourdeh <arminpourdehghan@gmail.com>
2016-10-31 15:46:55 -0700

    Timer function is added
    Timer for tree is added

--- src/game.js
+++ src/game.js
@@ -7,6 +7,17 @@ var carbNum = 0.00;
 var purchaseLevel = 1;
 var treeBasePrice = 10.00;
 
+
+var auto_adder = {
+    tree:0,
+    electric_car:0
+};
+
+var timer = {
+    tree:0,
+    electric_car:0
+};
+
 var element = {  
     
   clicker:document.getElementById("clicker"),
@@ -16,9 +27,21 @@ var element = {
 };
 
 element.clicker.onclick = function() { mainClicker(); };
-element.purchaseTree1.onclick = function() { treePriceCalc(); };
+element.purchaseTree1.onclick = function() {  
+    if(auto_adder.tree == 0) 
+    {
+        auto_adder.tree = 1;
+    }
+    else
+    {
+        auto_adder.tree += auto_adder.tree;
+    }
+    
+    treePriceCalc();
+};
 
 element.purchaseTree1.innerHTML = "Tree : unlock on " + treeBasePrice;
+
 
 function mainClicker()
 {
@@ -35,6 +58,34 @@ if(carbNum >= treeBasePrice)
 
 }
 
+setInterval(function(){ 
+    
+  if(auto_adder.tree > 0)
+  {
+     timer.tree += 1; 
+     autoTreeCarbNum();
+  }
+    
+ }, 1000);
+
+
+function autoTreeCarbNum () //timer for auto adition of the numbers of carbons  
+{
+    
+    if(timer.tree == 3) 
+    {
+       carbNum = carbNum + auto_adder.tree;
+       element.points.innerHTML = "Number Of Carbons: " + carbNum;
+       timer.tree = 0;
+    }
+    if(carbNum >= treeBasePrice)
+    {
+              document.getElementById("purchaseTree1").disabled = false; //these two dont work
+    }
+    else {
+              document.getElementById("purchaseTree1").disabled = true;
+    }
+}
 
 function treePriceCalc()
 {
@@ -44,18 +95,20 @@ function treePriceCalc()
     if (carbNum >= treeBasePrice)
     {
         
-        purchaseLevel++;
+        purchaseLevel++; //use this as a veriable in timer func
         
         //update total number of carbons
         carbNum =  carbNum - treeBasePrice;
         formatCarb = game_state.addCommas(carbNum);
         element.points.innerHTML = "Number Of Carbons: " + formatCarb;
         
+        
         //update number of carbons needed to buy tree 
         treeBasePrice = game_state.calcPrice(purchaseLevel, treeBasePrice);
         formatPrice = game_state.addCommas(treeBasePrice);
         element.purchaseTree1.innerHTML = "Tree : unlock on " + formatPrice;
-      
+        
+        
         if(carbNum < treeBasePrice)
           {
               document.getElementById("purchaseTree1").disabled = true;
